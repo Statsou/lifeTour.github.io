d891d0174ca3fd34a5fd87b966ef2606c1b2ffd8
Ник <110625824+Statsou@users.noreply.github.com>
2023-08-24 15:40:41 +0300

    Исправляет версию свайпера, исправляет проблему поломки кода при минификации

--- gulp/copyAssets.mjs
+++ gulp/copyAssets.mjs
@@ -13,7 +13,8 @@ const copy = () =>
     'source/**.html',
     'source/fonts/**',
     'source/img/**',
-    'source/favicon/**'
+    'source/favicon/**',
+    'source/js/swiper-new.js'
   ], {
     base: 'source',
   })
